9034a5a77db7021d27ea1c1e680fcb13e854515b
Adam Kopec <adam.kopec@gmail.com>
2013-04-21 23:17:58 +0200

    twig update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit958b450cb78c01a0c2c349392a7c5c59::getLoader();
+return ComposerAutoloaderInit6616b1abeac5d94f19426565ec2a981c::getLoader();
